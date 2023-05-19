e5689874fa41d674b77edf438d727fc47325c82a
Gelson Nobre <gelson.nobre@barezzi.com.br>
2023-05-19 16:25:44 -0300

    forms aula de sexta

---
+++ LocadoraClassic.View/FrmTelaPrincipal.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace LocadoraClassic.View
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMenu));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gêneroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filmeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.locaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.realizarLocaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatorioDeárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatóriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pendentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.faturamentoDoDiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fatramentoDoMêsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gráficosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gráficoPizzaDeClientePorEndereçoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrosToolStripMenuItem,
+            this.locaçãoToolStripMenuItem,
+            this.relatóriosToolStripMenuItem,
+            this.gráficosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            this.cadastrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gêneroToolStripMenuItem,
+            this.categoriaToolStripMenuItem,
+            this.filmeToolStripMenuItem,
+            this.clienteToolStripMenuItem});
+            this.cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // gêneroToolStripMenuItem
+            // 
+            this.gêneroToolStripMenuItem.Name = "gêneroToolStripMenuItem";
+            this.gêneroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gêneroToolStripMenuItem.Text = "Gênero";
+            this.gêneroToolStripMenuItem.Click += new System.EventHandler(this.gêneroToolStripMenuItem_Click);
+            // 
+            // categoriaToolStripMenuItem
+            // 
+            this.categoriaToolStripMenuItem.Name = "categoriaToolStripMenuItem";
+            this.categoriaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.categoriaToolStripMenuItem.Text = "Categoria";
+            this.categoriaToolStripMenuItem.Click += new System.EventHandler(this.categoriaToolStripMenuItem_Click);
+            // 
+            // filmeToolStripMenuItem
+            // 
+            this.filmeToolStripMenuItem.Name = "filmeToolStripMenuItem";
+            this.filmeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.filmeToolStripMenuItem.Text = "Filme";
+            // 
+            // clienteToolStripMenuItem
+            // 
+            this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // locaçãoToolStripMenuItem
+            // 
+            this.locaçãoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.realizarLocaçãoToolStripMenuItem,
+            this.relatorioDeárioToolStripMenuItem});
+            this.locaçãoToolStripMenuItem.Name = "locaçãoToolStripMenuItem";
+            this.locaçãoToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.locaçãoToolStripMenuItem.Text = "Locação";
+            // 
+            // realizarLocaçãoToolStripMenuItem
+            // 
+            this.realizarLocaçãoToolStripMenuItem.Name = "realizarLocaçãoToolStripMenuItem";
+            this.realizarLocaçãoToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.realizarLocaçãoToolStripMenuItem.Text = "Realizar Locação";
+            // 
+            // relatorioDeárioToolStripMenuItem
+            // 
+            this.relatorioDeárioToolStripMenuItem.Name = "relatorioDeárioToolStripMenuItem";
+            this.relatorioDeárioToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.relatorioDeárioToolStripMenuItem.Text = "Relatorio Diário";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            this.relatóriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pendentesToolStripMenuItem,
+            this.faturamentoDoDiaToolStripMenuItem,
+            this.fatramentoDoMêsToolStripMenuItem});
+            this.relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            this.relatóriosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // pendentesToolStripMenuItem
+            // 
+            this.pendentesToolStripMenuItem.Name = "pendentesToolStripMenuItem";
+            this.pendentesToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.pendentesToolStripMenuItem.Text = "Pendentes";
+            // 
+            // faturamentoDoDiaToolStripMenuItem
+            // 
+            this.faturamentoDoDiaToolStripMenuItem.Name = "faturamentoDoDiaToolStripMenuItem";
+            this.faturamentoDoDiaToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.faturamentoDoDiaToolStripMenuItem.Text = "Faturamento do Dia";
+            // 
+            // fatramentoDoMêsToolStripMenuItem
+            // 
+            this.fatramentoDoMêsToolStripMenuItem.Name = "fatramentoDoMêsToolStripMenuItem";
+            this.fatramentoDoMêsToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.fatramentoDoMêsToolStripMenuItem.Text = "Fatramento do Mês";
+            // 
+            // gráficosToolStripMenuItem
+            // 
+            this.gráficosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gráficoPizzaDeClientePorEndereçoToolStripMenuItem,
+            this.gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem});
+            this.gráficosToolStripMenuItem.Name = "gráficosToolStripMenuItem";
+            this.gráficosToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.gráficosToolStripMenuItem.Text = "Gráficos";
+            // 
+            // gráficoPizzaDeClientePorEndereçoToolStripMenuItem
+            // 
+            this.gráficoPizzaDeClientePorEndereçoToolStripMenuItem.Name = "gráficoPizzaDeClientePorEndereçoToolStripMenuItem";
+            this.gráficoPizzaDeClientePorEndereçoToolStripMenuItem.Size = new System.Drawing.Size(275, 22);
+            this.gráficoPizzaDeClientePorEndereçoToolStripMenuItem.Text = "Gráfico Pizza de Cliente por Endereço";
+            // 
+            // gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem
+            // 
+            this.gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem.Name = "gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem";
+            this.gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem.Size = new System.Drawing.Size(275, 22);
+            this.gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem.Text = "Gráfico de Barra de Atrasados por Mês";
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMenu";
+            this.Text = "LocadoraClassic";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gêneroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filmeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem locaçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem realizarLocaçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatorioDeárioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatóriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pendentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem faturamentoDoDiaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fatramentoDoMêsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gráficosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gráficoPizzaDeClientePorEndereçoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gráficoDeBarraDeAtrasadosPorMêsToolStripMenuItem;
+    }
+}
+
---
+++ LocadoraClassic.View/FrmTelaPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraClassic.View
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void gêneroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmTelaGenero frmTelaGenero = new FrmTelaGenero();
+            frmTelaGenero.Show();
+        }
+
+        private void categoriaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmTelaCategoria frmTelaCategoria = new FrmTelaCategoria();
+            frmTelaCategoria.Show();
+        }
+    }
+}
